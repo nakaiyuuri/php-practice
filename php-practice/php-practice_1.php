c70656cb2914c2b24ddd3ed70aa7feaab70f395a
nakai <nakaiyuuri0115@gmail.com>
2025-04-03 11:13:15 +0900

    Q4.Q8.Q9の解答

--- php-practice/php-practice_1.php
+++ php-practice/php-practice_1.php
@@ -1,51 +1,62 @@
 <?php
 // Q1 変数と文字列
 $name='中井';
-echo '私の名前は「'.$name.'」です。';
+echo '私の名前は「'.$name.'」です。'."\n";
 
 // Q2 四則演算
 $num=5*4;
 echo $num."\n";
 $num /= 2;
-echo $num;
+echo $num."\n";
 
 // Q3 日付操作
 date_default_timezone_set('Asia/Tokyo');
-echo '現在時刻は、'.date('Y年m月d日 H時i分s秒').'です。';
+echo '現在時刻は、'.date('Y年m月d日 H時i分s秒').'です。'."\n";
 
 
 // Q4 条件分岐-1 if文
-$device='windows';
-if($device=='windows')
-{
+$device='mac';
 
-    echo '使用OSは、windowsです。';
-} else if ($device=='mac'){
-    echo '使用OSは、macです。';
+if ($device==='windows'||$device==='mac') {
+    echo '使用OSは、'.$device.'です。'."\n";
 } else {
-    echo 'どちらでもありません。';
+    echo 'どちらでもありません。'."\n";
 }
 
 // Q5 条件分岐-2 三項演算子
 $age=7;
-echo ($age>=18)?'成人です。':'未成年です。';
+echo ($age>=18)?'成人です。'."\n":'未成年です。'."\n";
 
 // Q6 配列
 $array=['東京都','埼玉県','神奈川県','栃木県','千葉県','茨城県','山梨県','群馬県'];
-echo $array[3].'と'.$array[4].'は関東地方の都道府県です。';
+echo $array[3].'と'.$array[4].'は関東地方の都道府県です。'."\n";
 
 
 // Q7 連想配列-1
 $kanto=['東京都'=>'新宿区','神奈川県'=>'横浜市','千葉県'=>'千葉市','埼玉県'=>'さいたま市',
-        '栃木県'=>'宇都宮市','群馬県'=>'前橋市','茨城県'=>'宇都宮市'];
-foreach($kanto as $Y){
+        '栃木県'=>'宇都宮市','群馬県'=>'前橋市','茨城県'=>'水戸市'];
+foreach ($kanto as $Y) {
     echo $Y."\n";
 }
 
-// Q8 連想配列-2
+// Q8 連想配列-2      
+foreach ($kanto as $x=>$y) {
+    if ($x=='埼玉県') {
+        echo $x.'の県庁所在地は、'.$y.'です。'."\n";
+    }
+}
 
+// Q9 連想配列-3        
+$kanto['愛知県']='名古屋市';
+$kanto['大阪府']='大阪市';
 
-// Q9 連想配列-3
+foreach ($kanto as $x=>$y) {
+    if ($x!=='愛知県'&&$x!=='大阪府') {
+        echo $x.'の県庁所在地は'.$y.'です。'."\n";
+    } else {
+        echo $x.'は関東地方ではありません。'."\n";
+    }
+}
 
 
 // Q10 関数-1
@@ -58,7 +69,7 @@ echo hello('安藤');
 
 // Q11 関数-2
 function calcTaxlnPrice($price){
-    return $price.'円の商品の税込み価格は'.$price*1.1.'です。';
+    return $price.'円の商品の税込み価格は'.$price*1.1.'です。'."\n";
 }
 
 $taxlnPrice=calcTaxlnPrice(1000);
@@ -66,13 +77,11 @@ echo $taxlnPrice;
 
 // Q12 関数とif文
 function distinguishNum($num){
-  if($num%2==0){
-      
-  return $num.'は偶数です。'."\n";
-      
-  }else{return $num.'は奇数です。'."\n";
-
-  }    
+    if ($num%2===0) {
+        return $num.'は偶数です。'."\n";
+        } else {
+        return $num.'は奇数です。'."\n";
+        }    
 }
 
 echo distinguishNum(11);
@@ -81,28 +90,26 @@ echo distinguishNum(24);
 
 // Q13 関数とswitch文
 function evaluateGrade($grade){
-    switch($grade){
+    switch ($grade) {
 
-      case 'A':
-      case 'B':
-      return '合格です。'."\n";
-      break;
+        case 'A':
+        case 'B':
+            return '合格です。'."\n";
+            break;
       
-      case 'C':
-      return '合格ですが追加課題があります。'."\n";
-      break;
+        case 'C':
+            return '合格ですが追加課題があります。'."\n";
+            break;
       
-      case 'D':
-      return '不合格です。'."\n";
-      break;
+        case 'D':
+            return '不合格です。'."\n";
+            break;
       
-      default:
-      return '判定不明です。講師に問いあわせてください'."\n";
-      break;
+        default:
+            return '判定不明です。講師に問いあわせてください'."\n";
+            break;
     }
 }
+
 echo evaluateGrade('A');
 echo evaluateGrade('L');
-
-
-?>
