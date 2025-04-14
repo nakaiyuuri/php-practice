1bfa31680175cc5c846ed1b1dd0791be48b44ba3
nakai <nakaiyuuri0115@gmail.com>
2025-04-14 16:06:35 +0900

    練習問題2のQ4まで

--- php-practice/php-practice_2.php
+++ php-practice/php-practice_2.php
@@ -1,15 +1,111 @@
 <?php
 // Q1 tic-tac問題
-
+echo '1から100までのカウントを開始します'."\n\n";
+for ($i=1;$i<=100;$i++) {
+    if ($i%4===0 && $i%5===0) {
+        echo 'tic-tac'."\n";
+    } elseif ($i%4===0) {
+        echo 'tic'."\n";
+    } elseif ($i%5===0) {
+        echo 'tac'."\n";
+    } else {
+        echo $i."\n";
+    }
+}
 
 // Q2 多次元連想配列
 
+//問題１
+$personalInfos = [
+  [
+      'name' => 'Aさん',
+      'mail' => 'aaa@mail.com',
+      'tel'  => '09011112222'
+  ],
+  [
+      'name' => 'Bさん',
+      'mail' => 'bbb@mail.com',
+      'tel'  => '08033334444'
+  ],
+  [
+      'name' => 'Cさん',
+      'mail' => 'ccc@mail.com',
+      'tel'  => '09055556666'
+  ],
+];
+
+echo $personalInfos[1]['name'].'の電話番号は'.$personalInfos[1]['tel'].'です。'."\n";
+
+//問題２
+foreach ($personalInfos as $X => $Y) {
+    echo 1+$X.'番目の'.$Y['name'].'のメールアドレスは'.$Y['mail'].'で、電話番号は'.$Y['tel'].'です。'."\n";
+}
+
+//問題３
+$ageList=[25,30,18];
+foreach ($personalInfos as $X => $Y) {
+    $personalInfos[$X]['age']=$ageList[$X];
+}
+
+var_dump($personalInfos);
 
 // Q3 オブジェクト-1
+class Student
+{
+    public $studentId;
+    public $studentName;
 
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+    
+    public function IdName()
+    {
+        echo '学籍番号'.$this->studentId.'番の生徒は'.$this->studentName.'です。'."\n";
+    }
 
-// Q4 オブジェクト-2
+    public function attend()
+    {
+        echo '授業に出席しました。';
+    }
+}
+
+$yamada=new Student(120, '山田');
+$yamada->IdName();
+
+// Q4 オブジェクト-2(attend()の書き換え)
+class Student
+{
+    public $studentId;
+    public $studentName;
+
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+    
+    public function IdName()
+    {
+        echo '学籍番号'.$this->studentId.'番の生徒は'.$this->studentName.'です。'."\n";
+    }
+
+    public function attend($class)
+    {
+        echo $this->studentName.'は'.$class.'の授業に参加しました。学籍番号：'.$this->studentId."\n";
+    }
+}
+
+$yamada=new Student(120, '山田');
+$yamada->IdName();
+$yamada->attend('PHP');
 
 
 // Q5 定義済みクラス
+//問題１
+
+
+//問題２
 ?>
