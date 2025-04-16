2c2482b00303cec25dde52988c6304d043550aac
nakai <nakaiyuuri0115@gmail.com>
2025-04-16 12:33:37 +0900

    update: php練習問題2,Q2,問題1 インデントを揃える

--- php-practice/php-practice_2.php
+++ php-practice/php-practice_2.php
@@ -17,21 +17,21 @@ for ($i = 1; $i <= 100; $i ++) {
 
 //問題１
 $personalInfos = [
-  [
-      'name' => 'Aさん',
-      'mail' => 'aaa@mail.com',
-      'tel'  => '09011112222'
-  ],
-  [
-      'name' => 'Bさん',
-      'mail' => 'bbb@mail.com',
-      'tel'  => '08033334444'
-  ],
-  [
-      'name' => 'Cさん',
-      'mail' => 'ccc@mail.com',
-      'tel'  => '09055556666'
-  ],
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
 ];
 
 echo $personalInfos[1]['name'] . 'の電話番号は' . $personalInfos[1]['tel'] . 'です。' . "\n";
